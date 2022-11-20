3d9fec248c4fc8574a0b5afc81ebce63c0a16571
Jewel James <53769636+jamjewel@users.noreply.github.com>
2022-11-20 21:33:14 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -225,10 +225,10 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+   /*     if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
+        } else { */
             $.post('https://script.google.com/macros/s/AKfycbyl06TlFlaSQ2yK-3QK58_EN5fpWMQO7b_VsFkMqS_rZW7mYCIEUfjNAjxlydMPMzWkEg/exec', data)
                 .done(function (data) {
                     console.log(data);
